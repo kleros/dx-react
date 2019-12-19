36dd8346fd30d085e481a5993b42a06ba89ba5d2
Matheus Alencar <malencar@pm.me>
2019-12-19 13:08:42 -0300

    fix: swap ethereum.enable for eth_requestAccounts

--- src/api/web3Provider.ts
+++ src/api/web3Provider.ts
@@ -13,7 +13,7 @@ export const setupWeb3 = async (provider?: Provider) => {
       const providerWeb3 = window.web3 = new Web3(window.ethereum)
       try {
           // Request account access if needed
-        await window.ethereum.enable()
+        await window.ethereum.send({ method: 'eth_requestAccounts' })
         return providerWeb3
       } catch (error) {
           // User denied account access...
